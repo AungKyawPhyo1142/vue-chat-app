67588d3687ee9547bf900a85ba6043cf8493251a
Aung Kyaw Phyo (Sebastian Kein) <94116245+AungKyawPhyo1142@users.noreply.github.com>
2023-02-08 11:36:49 +0630

    init project setup vue project & template UI

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,7 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: ()=>import('../views/PrivateChat.vue')
   },
   {
     path: '/about',
@@ -14,7 +14,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
 ]
 
 const router = createRouter({
